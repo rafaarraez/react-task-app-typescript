b97446785ba120fd4898274b902684b575ca14c7
rafael <rafa.arraez.gue@gmail.com>
2021-01-04 16:27:43 -0400

    Updates

---
+++ precache-manifest.8158b7cfd0edbba03cdb500f7ff7e3fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd8485c2e9da30ca9c2dbc3837368e5",
+    "url": "/react-task-app-typescript/index.html"
+  },
+  {
+    "revision": "8efc324df78ad4f590c3",
+    "url": "/react-task-app-typescript/static/css/2.acc62604.chunk.css"
+  },
+  {
+    "revision": "7fa3e12711ec63e7f570",
+    "url": "/react-task-app-typescript/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8efc324df78ad4f590c3",
+    "url": "/react-task-app-typescript/static/js/2.0cdeb59e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-task-app-typescript/static/js/2.0cdeb59e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fa3e12711ec63e7f570",
+    "url": "/react-task-app-typescript/static/js/main.c58690b7.chunk.js"
+  },
+  {
+    "revision": "f6eb173b93e7920fe77e",
+    "url": "/react-task-app-typescript/static/js/runtime-main.2da7a20a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-task-app-typescript/precache-manifest.8158b7cfd0edbba03cdb500f7ff7e3fd.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-task-app-typescript/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
